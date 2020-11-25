0b7364a267f5d0da508cf0ef2dd0cfb25903ba64
laur80 <laur80raul@gmail.com>
2020-11-25 12:37:10 +0200

    fun-v-update

--- src/Card.js
+++ src/Card.js
@@ -1,17 +1,18 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 import "./Card.css";
 
-function Card(props){
-  
-    let angle = Math.random() * 90 - 45;
-    let xPos = Math.random() * 40 - 20;
-    let yPos = Math.random() * 40 - 20;
-  let  _transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`;
-  
-  
+ 
+  function Card(props){
+
+    let angle = useState(Math.random() * 90 - 45)[0];
+    let xPos = useState(Math.random() * 40 - 20)[0];	   
+    let yPos = useState(Math.random() * 40 - 20)[0];
+    let transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`;
+
+ 
     return (
       <img
-        style={{ transform: _transform }}
+        style={{ transform: transform }}
         className='Card'
         src={props.image}
         alt={props.name}
