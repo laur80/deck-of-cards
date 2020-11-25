00f2f4ba2e4a55fdb10a3834693dd639f13ba524
laur80 <laur80raul@gmail.com>
2020-11-25 13:03:10 +0200

    fun-v-up

---
+++ build/precache-manifest.ccd8baeed3f66b7f04d1c5145e2ec22b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb43927d8783368d6aaa",
+    "url": "/deck-of-cards/static/css/main.90cf04b8.chunk.css"
+  },
+  {
+    "revision": "fb43927d8783368d6aaa",
+    "url": "/deck-of-cards/static/js/main.fb43927d.chunk.js"
+  },
+  {
+    "revision": "626bd4be2f18e7f1f47e",
+    "url": "/deck-of-cards/static/js/1.626bd4be.chunk.js"
+  },
+  {
+    "revision": "f317f0df2629cb3a97ae",
+    "url": "/deck-of-cards/static/js/runtime~main.f317f0df.js"
+  },
+  {
+    "revision": "51f3d52e26a9b2cb6bd0228857329ba1",
+    "url": "/deck-of-cards/index.html"
+  }
+];
